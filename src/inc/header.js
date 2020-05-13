85d239b8568d22405e23dbee685fc93a1813637f
bong9779 <ranch0104@naver.com>
2020-05-14 03:41:18 +0900

    5 id pw show

--- src/inc/header.js
+++ src/inc/header.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
 import '../App.css';
+import Modal from 'react-awesome-modal';
 class header extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+          visible : false,
+          id : "",
+          password : ""
+      }
+  }  
+  _openModal = function() {
+    this.setState({
+        visible : true
+    });
+  }
+
+  _closeModal = function() {
+    this.setState({
+        visible : false
+    });
+  }
+  _changeID = function() {
+    const id_v = document.getElementsByName('id')[0].value;
+
+    this.setState({
+        id : id_v
+    });
+  }
+  _changePW = function() {
+    const pw_v = document.getElementsByName('password')[0].value;
+
+    this.setState({
+        password : pw_v
+    });
+  }
+
 
   render() {
-
+    console.log('아이디: ' + this.state.id + ' 비밀번호: '+this.state.password);
     return (
         <div class='header_grid'>
             <div> </div>
@@ -14,7 +49,34 @@ class header extends Component {
             </div>
 
             <div className='acenter'> 
-                <h5> LOGIN </h5>
+            <h5 onClick={() => this._openModal()}> LOGIN </h5>
+            <Modal visible={this.state.visible} 
+                       width="400" height="360"
+                       effect="fadeInDown" 
+                       onClickAway={() => this._closeModal()}
+                >
+                  <div>
+                    <h4 className='acenter login_tit'> LOGIN </h4>
+                    <form>
+                    <div className='login_div'>
+                      <div className='login_input_div'>
+                        <p> ID </p>
+                        <input type='text' name='id' onChange={() => this._changeID()}/>
+                      </div>
+
+                      <div className='login_input_div' style={{ 'marginTop' : '40px'}}>
+                        <p> Password </p>
+                        <input type='text' name='password' onChange={()=>this._changePW()}/>
+                      </div>
+
+                      <div className='submit_div'>
+                        <div> <input type='button' value='로그인'/> </div>
+                        <div> <input type='button' value='취소' onClick={() => this._closeModal()}/> </div>
+                      </div>
+                    </div>
+                    </form>
+                  </div>
+                </Modal>
             </div>
         </div>
     );
